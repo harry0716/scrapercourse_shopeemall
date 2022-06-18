ce4a0640a9e9dde9baf21b7c1dc72d2cb97afca8
harry0716 <harry0716@gmail.com>
2022-06-18 17:49:30 +0800

    5.6 批次爬取子網頁的資料

--- scraper.py
+++ scraper.py
@@ -16,12 +16,26 @@ cards = driver.find_elements(By.CSS_SELECTOR, "div[class='col-xs-2 recommend-pro
 
 items = []
 for card in cards:
-    ActionChains(driver).move_to_element(card).perform()  #自動化移動滑鼠到每一個商品區塊元素
+    #ActionChains(driver).move_to_element(card).perform()  #自動化移動滑鼠到每一個商品區塊元素
 
     title = card.find_element(By.CSS_SELECTOR, "div[class='ie3A+n bM+7UW Cve6sh']").text
     price = card.find_element(By.CSS_SELECTOR, "div[class='vioxXd rVLWG6']").text
     link = card.find_element(By.TAG_NAME,"a").get_attribute('href')
     items.append((title,price,link))
 
-print(items)
+#print(items)
 
+result = []
+for item in items:  #items的第三個抓取物件，就是商品的超連結(link)，就是子網頁連結。
+    driver.get(item[2])  #子網頁連結在索引值2
+
+    for i in range(5):
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")  #滾動滑鼠
+        time.sleep(3)   #滑鼠滾動後設定等待時間
+
+    comments = driver.find_elements(By.CSS_SELECTOR, "div[class='Em3Qhp'")  #用定位元素，尋找所有class屬性值為Em3Qhp的物件
+    for comment in comments:
+        result.append((item[0],item[1],comment.text))  #用text屬性爬取評價上的資料
+    break
+
+print(result)
