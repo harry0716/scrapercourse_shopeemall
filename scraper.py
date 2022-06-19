1cb7c50b624f4a6097986ba485b86f80d97944b8
harry0716 <harry0716@gmail.com>
2022-06-19 12:50:43 +0800

    5.7 批次爬取多分頁的網頁資料(作業)

--- scraper.py
+++ scraper.py
@@ -6,36 +6,42 @@ from selenium.webdriver.common.action_chains import ActionChains #導入selenium
 
 driver = webdriver.Chrome(ChromeDriverManager().install()) 
 
-driver.get('https://shopee.tw/mall/%E6%9B%B8%E7%B1%8D%E5%8F%8A%E9%9B%9C%E8%AA%8C%E6%9C%9F%E5%88%8A-cat.11041120')  
-time.sleep(5)
+for page in range(1, 3):
 
-ActionChains(driver).move_by_offset(100,100).click().perform()  #用ActionChains動作鏈模組移動滑鼠到(100,100)的位置
-                                                                #，呼叫click方法點擊屬左鍵，在呼叫perform方法執行。
+    driver.get(
+        f'https://shopee.tw/mall/%E6%9B%B8%E7%B1%8D%E5%8F%8A%E9%9B%9C%E8%AA%8C%E6%9C%9F%E5%88%8A-cat.11041120/popular?pageNumber={page}')  
+    time.sleep(5)
 
-cards = driver.find_elements(By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
+    ActionChains(driver).move_by_offset(100,100).click().perform()  #用ActionChains動作鏈模組移動滑鼠到(100,100)的位置
+                                                                    #，呼叫click方法點擊屬左鍵，在呼叫perform方法執行。
 
-items = []
-for card in cards:
-    #ActionChains(driver).move_to_element(card).perform()  #自動化移動滑鼠到每一個商品區塊元素
+    cards = driver.find_elements(By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
 
-    title = card.find_element(By.CSS_SELECTOR, "div[class='ie3A+n bM+7UW Cve6sh']").text
-    price = card.find_element(By.CSS_SELECTOR, "div[class='vioxXd rVLWG6']").text
-    link = card.find_element(By.TAG_NAME,"a").get_attribute('href')
-    items.append((title,price,link))
+    items = []
+    for card in cards:
+        #ActionChains(driver).move_to_element(card).perform()  #自動化移動滑鼠到每一個商品區塊元素
 
-#print(items)
+        title = card.find_element(By.CSS_SELECTOR, "div[class='ie3A+n bM+7UW Cve6sh']").text
+        price = card.find_element(By.CSS_SELECTOR, "div[class='vioxXd rVLWG6']").text
+        link = card.find_element(By.TAG_NAME,"a").get_attribute('href')
+        items.append((title,price,link))
 
-result = []
-for item in items:  #items的第三個抓取物件，就是商品的超連結(link)，就是子網頁連結。
-    driver.get(item[2])  #子網頁連結在索引值2
+    #print(items)
 
-    for i in range(5):
-        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")  #滾動滑鼠
-        time.sleep(3)   #滑鼠滾動後設定等待時間
+    result = []
+    for item in items:  #items的第三個抓取物件，就是商品的超連結(link)，就是子網頁連結。
+        driver.get(item[2])  #子網頁連結在索引值2
 
-    comments = driver.find_elements(By.CSS_SELECTOR, "div[class='Em3Qhp'")  #用定位元素，尋找所有class屬性值為Em3Qhp的物件
-    for comment in comments:
-        result.append((item[0],item[1],comment.text))  #用text屬性爬取評價上的資料
-    
+        for i in range(5):
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")  #滾動滑鼠
+            time.sleep(3)   #滑鼠滾動後設定等待時間
 
-print(result)
+        comments = driver.find_elements(By.CSS_SELECTOR, "div[class='Em3Qhp'")  #用定位元素，尋找所有class屬性值為Em3Qhp的物件
+        for comment in comments:
+            result.append((item[0],item[1],comment.text))  #用text屬性爬取評價上的資料
+        break
+
+    print(f"第{page}頁")
+    print(result)
+
+
